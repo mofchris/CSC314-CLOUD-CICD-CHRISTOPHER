dea916b72056f1776fe54c2ffda705ddce9d453a
mofchris <77124954+mofchris@users.noreply.github.com>
2024-05-29 17:50:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const port = 8080;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define the root route
+// Define the root route to serve the index.html file
 app.get('/', (req, res) => {
-    res.send('Welcome to the Home Page');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Define the '/about' route
 app.get('/about', (req, res) => {
-    res.send('THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS.  MOFUNANYA CHRISTOPHER ----------------- VUG/CSC/21/5659');
+    res.send('THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. MOFUNANYA CHRISTOPHER VUG/CSC/21/5659');
 });
 
 // Start the server
