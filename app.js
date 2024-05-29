2933daa5dc44e9558ed90cb910c0f76b019ddcb3
mofchris <77124954+mofchris@users.noreply.github.com>
2024-05-29 12:05:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use(express.static('public'));
-//app.get('/', (req, res) => res.send(THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS BY MOFUNANYA CHRISTOPHER VUG/CSC/21/5659. THIS PAGE INDICATES SUCCESS. '));
+//app.use(express.static('public'));
+app.get('/', (req, res) => res.send(THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS BY MOFUNANYA CHRISTOPHER VUG/CSC/21/5659. THIS PAGE INDICATES SUCCESS. '));
 
 
 
