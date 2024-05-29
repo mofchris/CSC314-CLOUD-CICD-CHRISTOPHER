077a48f202dae5871c046b7a2fcb294be155e8c7
mofchris <77124954+mofchris@users.noreply.github.com>
2024-05-29 17:05:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,27 +6,13 @@ const port = 8080;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'app.js'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Assuming you have an index.html file in the 'public' directory
 });
 
 app.get('/about', (req, res) => {
-    res.send('THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS BY MOFUNANYA CHRISTOPHER VUG/CSC/21/5659. THIS PAGE INDICATES SUCCESS. '));
+    res.send('THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS.  MOFUNANYA CHRISTOPHER ----------------- VUG/CSC/21/5659');
 });
-
-
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
